79a95d155356d6a4e48794bd327790fb492167a1
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-28 23:41:59 +0530

    footer.tsx updated

--- cake_ordering-system/src/components/layout/footer.tsx
+++ cake_ordering-system/src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ function Footer(): JSX.Element {
       <div className=" bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E] text-black rounded-t-3xl mt-8 md:mt-0">
         <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
           <div className=" w-full md:w-1/4">
-            <h1 className=" font-semibold text-xl pb-4">CafePulse</h1>
+            <h1 className=" font-semibold text-xl pb-4">DelishCake</h1>
             <p className=" text-sm">
               Welcome to our coffee haven! Explore our aromatic brews, savor
               artisanal flavors, and discover the perfect roast to elevate your
@@ -91,10 +91,10 @@ function Footer(): JSX.Element {
         <div>
           <p>
             <p className=" text-center py-4">
-              @copyright developed by
+              @copyright2023 developed by
               <span className=" text-backgroundColor">
               {" "}
-                champion programmers{" "}
+                tharuka wijesekara{" "}
             </span>
               | All rights reserved
             </p>
