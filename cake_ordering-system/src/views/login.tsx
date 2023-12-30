ad8b8b85fea7c0c11445762222b29be321470f76
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-31 04:17:21 +0530

    login.tsx update

--- cake_ordering-system/src/views/login.tsx
+++ cake_ordering-system/src/views/login.tsx
@@ -6,16 +6,16 @@ class Login extends React.Component<any, any> {
 
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
     return(
-      <section className={'flex justify-center items-center p-5'}>
-        <div className={'w-fit p-24 border shadow-xl rounded-xl'}>
+      <section className={'flex justify-center items-center p-5 bg-pink-100'}>
+        <div className={'w-fit p-24 border shadow-xl rounded-xl bg-white font-bold text-black'}>
 
           <img src="/src/assets/logo.jpg" title="logo" alt="logo" className={'w-24 m-auto'}/>
 
-          <div className={'text-2xl font-bold text-amber-950 text-center mt-5'}>
+          <div className={'text-2xl font-bold text-black text-center mt-5'}>
             Sign In
           </div>
 
-          <div className={'mt-5 min-w-[300px] font-bold text-amber-950'}>
+          <div className={'mt-5 min-w-[300px] font-bold text-black'}>
 
             <Input
               type={'user name'}
@@ -33,12 +33,12 @@ class Login extends React.Component<any, any> {
 
           </div>
 
-          <div className={'text-center mt-5'}>
+          <div className={'text-center mt-5 text-white'}>
             <button className={'main-btn '}>Sign In</button>
           </div>
 
           <div className={'text-center mt-5 '}>
-            Do not have an account? <Link to={'/signup'}><span className={'text-white-100 italic'}>Sign up now</span></Link>
+            Do not have an account? <Link to={'/signup'}><span className={'italic underline'}>Sign up now</span></Link>
           </div>
 
         </div>
