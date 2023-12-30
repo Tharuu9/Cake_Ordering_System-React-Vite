5e5fa6ed4d1ea8f4bd0cddcda3be25afbd03c236
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-31 04:16:49 +0530

    button.tsx update

--- cake_ordering-system/src/components/card/button.tsx
+++ cake_ordering-system/src/components/card/button.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Button = (props) => {
     return (
         <div>
-            <button className="px-6 py-1 border-2 border-white bg-[#FFDCAB] hover:text-[#AB6B2E] transition-all rounded-full">
+            <button className="px-6 py-1 border-2 border-white bg-blue hover:text-black transition-all rounded-full">
                 {props.title}
             </button>
         </div>
