da27d4b1115bbcfeb8642e5b413f99034751958e
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2024-01-05 13:24:16 +0530

    AboutPage.tsx update

--- cake_ordering-system/src/views/AboutPage.tsx
+++ cake_ordering-system/src/views/AboutPage.tsx
@@ -6,15 +6,15 @@ class MenuPage extends React.Component<any, any>{
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
         return(
-        <div className="min-h-screen flex flex-col  lg:px-32 px-5  bg-pink-100">
-            <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8 font-bold text-amber-950 italic">About Us</h1>
+        <div className="min-h-screen flex flex-col  lg:px-32 px-5  bg-white">
+            <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8 font-bold text-black italic">About Us</h1>
 
             <div className="flex flex-col lg:flex-row items-center gap-5">
                 <div className="w-full lg:w-2/4">
                     <img className="rounded-lg" src="/src/assets/about.png" alt="img"/>
                 </div>
                 <div className="w-full lg:w-2/4 p-4 space-y-3  items-center justify-center">
-                    <h2 className="font-semibold text-3xl items-center justify-center font-bold text-amber-950 italic">
+                    <h2 className="font-semibold text-3xl items-center justify-center font-bold text-black italic">
                         What Makes Our Coffee Cake Special?
                     </h2>
                     <p>
